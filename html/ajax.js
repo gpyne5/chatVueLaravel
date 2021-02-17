55d3f0aa5a1b2be7e29eec4685410ad8b33cae2d
gpyne5 <w5000c2d26@gmail.com>
2021-02-17 15:08:17 +0900

    投稿フォームを作成。次はモデルとの紐付け

--- html/ajax.js
+++ html/ajax.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
-    let result = document.getElementById('result');
     let xhr = new XMLHttpRequest();
+
+    let result = document.getElementById('result');
+    let myMsg = document.getElementById('mymsg');
+    let otherMsg = document.getElementById('othermsg');
+    let commentsLen = 0; //差分更新のためのカウンター
+    let createCommentLen = 0;
+    
+    
     xhr.onreadystatechange = function(){
         if (xhr.readyState === 4){
             if (xhr.status === 200){
@@ -8,23 +15,43 @@ document.addEventListener('DOMContentLoaded', function(){
                 if (data === null) {
                     result.textContent = '何も取得できませんでした';
                 } else {
-                    let ul = document.createElement('ul');
-                    for (let i = 0, len = data.length; i < len; i ++) {
-                        let li = document.createElement('li');
-                        let msg = document.createTextNode(data[i].msg);
-                        li.appendChild(msg);
-                        ul.appendChild(li);
+                    let user = data[1]; //ログインしているユーザー
+                    let comments = data[0]; //コメントデータ
+                    document.getElementById('user_id').value = user.id;
+
+                    if (commentsLen === 0 || commentsLen !== comments.length){ //最初の1回目もしくはコメント数がカウンターと違ってたら
+                        createCommentLen = comments.length - commentsLen; //更新すべき件数
+                        commentsLen = comments.length; //差分更新のためのカウンターの更新
+                        for (let i = 0, j = 1; i < createCommentLen; i ++, j ++) {
+                            let li = document.createElement('li');
+                            let msg = document.createTextNode(comments[commentsLen - j].msg); //Array[-1]みたいに後ろから指定できないので総数-1で一番うしろを指定
+                            
+                            //自分のコメントか他の人のコメントか判定
+                            if (comments[commentsLen - j].user_id === user.id){ //restdataデータベースの user_id　と user.id　を比較
+                                li.appendChild(msg);
+                                myMsg.appendChild(li);
+                            } else {
+                                li.appendChild(msg);
+                                otherMsg.appendChild(li);
+                            }
+                        }
                     }
-                    result.replaceChild(ul, result.firstChild);
+                    result.textContent = '';
                 }
             } else {
                 result.textContent = "サーバーエラーが発生しました";
             }
         }else{
-            result.textContent = '通信中う　...';
+            result.textContent = '';
         }
     };
+    document.getElementById('submit').addEventListener('click', function(){
+        xhr.open('GET', '../rest/')
+    })
+
+    setInterval(function(){
         xhr.open('GET', '../rest', true);
         xhr.send(null);
-
+    }, 5000);
+    
 }, false);
